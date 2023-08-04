a343800eb07f9b0cb8d1af8abefc1bb98e161aac
souravbsk <souravbsk01@gmail.com>
2023-08-05 04:29:28 +0600

    link added

--- src/components/ProductDiv.js
+++ src/components/ProductDiv.js
@@ -4,49 +4,52 @@ import React from "react";
 import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
 import { FaRegHeart } from "react-icons/fa";
+import Link from "next/link";
 const ProductDiv = ({ product }) => {
-  const { thumbnail, title, price, description, rating } = product || {};
+  const { id, thumbnail, title, price, description, rating } = product || {};
   return (
-    <div className="flex h-[550px] relative border gap-5 rounded-lg overflow-hidden flex-col">
-      <div className="bg-[#F5F6F6] ">
-        <Image
-          src={thumbnail}
-          className="w-full h-[280px]"
-          width={325}
-          height={280}
-          alt="thumbnail"
-        ></Image>
-      </div>
-      <div className="p-2 flex flex-col justify-between gap-2">
-        <div className="flex mt-auto gap-4 justify-between">
-          <h2 className="text-lg font-bold">{title}</h2>
-          <p className="text-lg font-bold">
-            <sup>$</sup>
-            {price}
-            <sup>.00</sup>
-          </p>
+    <Link href={`/product-details/${id}`}>
+      <div className="flex h-[550px] relative border gap-5 rounded-lg overflow-hidden flex-col">
+        <div className="bg-[#F5F6F6] ">
+          <Image
+            src={thumbnail}
+            className="w-full h-[280px]"
+            width={325}
+            height={280}
+            alt="thumbnail"
+          ></Image>
         </div>
-        <div className="absolute space-y-3 pb-3 bottom-0 ">
-          <p>
-            {description.length > 30 ? description.slice(0, 30) : description}
-          </p>
-          <div>
-            <Rating
-              className="text-[#003D2A]"
-              readOnly
-              style={{ maxWidth: 100 }}
-              value={rating}
-            />
+        <div className="p-2 flex flex-col justify-between gap-2">
+          <div className="flex mt-auto gap-4 justify-between">
+            <h2 className="text-lg font-bold">{title}</h2>
+            <p className="text-lg font-bold">
+              <sup>$</sup>
+              {price}
+              <sup>.00</sup>
+            </p>
           </div>
-          <button className="rounded-3xl font-semibold hover:bg-[#003D2A] hover:text-white duration-300 border-2  border-[#003D2A] text-[#003D2A] px-4 py-2">
-            Add to Cart
-          </button>
+          <div className="absolute space-y-3 pb-3 bottom-0 ">
+            <p>
+              {description.length > 30 ? description.slice(0, 30) : description}
+            </p>
+            <div>
+              <Rating
+                className="text-[#003D2A]"
+                readOnly
+                style={{ maxWidth: 100 }}
+                value={rating}
+              />
+            </div>
+            <button className="rounded-3xl font-semibold hover:bg-[#003D2A] hover:text-white duration-300 border-2  border-[#003D2A] text-[#003D2A] px-4 py-2">
+              Add to Cart
+            </button>
+          </div>
         </div>
+        <p className="absolute right-5 bg-white px-2 py-2 rounded-full inline-block cursor-pointer text-2xl top-5">
+          <FaRegHeart></FaRegHeart>
+        </p>
       </div>
-      <p className="absolute right-5 bg-white px-2 py-2 rounded-full inline-block cursor-pointer text-2xl top-5">
-        <FaRegHeart></FaRegHeart>
-      </p>
-    </div>
+    </Link>
   );
 };
 
